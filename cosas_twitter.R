ed514f64338110e39a795f821f9d5921f50082a9
Juan Sokil <juanpablosokil@gmail.com>
2018-06-12 17:01:53 -0300

    no message

--- cosas_twitter.R
+++ cosas_twitter.R
@@ -12,18 +12,41 @@ library(maps)
 
 
 
-
 setwd("~/Emotion_Analysis_TM")
 source('./my_oauth.r', encoding = 'latin1')
 
 #tw = twitteR::searchTwitter('#VotenAbortoLegal + #SalvemosLas2Vidas', n = 20, since = '2018-06-12 00:22:51', until='2016-06-12 02:22:51', retryOnRateLimit = 1e3)
 #tweets.df <- twListToDF(tw)
 
-filterStream(file.name="./tweets/tweets_abortolegal.json", locations = c(-72,-55,-55,-22), language = "es", track = "AbortoLegal", timeout = 100,  oauth = my_oauth)
-filterStream(file.name="./tweets/tweets_2vidas.json", locations = c(-72,-55,-55,-22),language = "es",  track = "SalvemosLas2Vidas", timeout = 100,  oauth = my_oauth)
+
+#SalvemosLas2Vidas
+#AbortoLegalYa
+#AbortoLegal
+#AbortoLegalSeguroYGratuito
+#AbortoSinBarreras
+#13JAbortoLegal
+#VotenAbortoLegal
+#NoAlAborto
+#SiALaVida
+#MentiraVerde
+#ArgentinaEsProVida
+#ElijamosLas2Vidas
+
+i=1
+for (i in range(10)){
+  filterStream(file.name="./tweets/tweets_abortolegal.json", locations = c(-72,-55,-55,-22), language = "es", track = c("#AbortoLegalYa","#AbortoLegalSeguroYGratuito","#13JAbortoLegal","#VotenAbortoLegal"), timeout = 3,  oauth = my_oauth)
+}
+
+
+i=1
+for (i in range(10)){
+filterStream(file.name="./tweets/tweets_2vidas.json", locations = c(-72,-55,-55,-22),language = "es",  track = c("#SalvemosLas2Vidas","#ArgentinaEsProVida","#SiALaVida","#NoAlAborto"), timeout = 30,  oauth = my_oauth)
+}
+
 
 tweetslegal.df <- parseTweets("./tweets/tweets_abortolegal.json", verbose = FALSE)
 tweets2vidas.df <- parseTweets("./tweets/tweets_2vidas.json", verbose = FALSE)
+
 
 
 map.data <- map_data("world")
@@ -35,8 +58,8 @@ ggplot(map.data) + geom_map(aes(map_id = region), map = map.data, fill = "white"
         axis.title = element_blank(), panel.background = element_blank(), panel.border = element_blank(), 
         panel.grid.major = element_blank(), plot.background = element_blank(), 
         plot.margin = unit(0 * c(-1.5, -1.5, -1.5, -1.5), "lines")) + 
-  geom_point(data = points, aes(x = x, y = y), size = 2, alpha = 1, color = "green") +
-geom_point(data = points2, aes(x = x, y = y), size = 2, alpha = 1, color = "blue")
+  geom_point(data = points, aes(x = x, y = y), size = 3, alpha = 0.6, color = "dark green") +
+geom_point(data = points2, aes(x = x, y = y), size = 2, alpha = 0.6, color = "light blue")
 
 
 
--- my_oauth.R
+++ my_oauth.R
@@ -10,9 +10,18 @@ accessURL <- "https://api.twitter.com/oauth/access_token"
 authURL <- "https://api.twitter.com/oauth/authorize"
 consumerKey <- "rYct9vETBZJ9aRFiBMrfqHfRD"
 consumerSecret <- "c2sjBrxR6sJpOx9vXkNj3gaxI7O9lbWi4rx8opdD90zVuVZCRM"
+
+
+
+setup_twitter_oauth(api_key, api_secret, access_token, access_token_secret)
+
+
+
 my_oauth <- OAuthFactory$new(consumerKey=consumerKey,
                              consumerSecret=consumerSecret, requestURL=requestURL,
                              accessURL=accessURL, authURL=authURL)
 
 my_oauth$handshake(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))
+
+
 
