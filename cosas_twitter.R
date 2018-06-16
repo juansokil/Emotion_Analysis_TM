44d1ce4f82cc1720d63c16c9f2a90baa5fce98f6
LAPTOP-15DHH6HT\Juan <juanpablosokil@gmail.com>
2018-06-16 09:09:03 -0300

    Clean

--- cosas_twitter.R
+++ cosas_twitter.R
@@ -1,6 +1,6 @@
 #install.packages("twitteR")
 #install.packages("streamR")
-#install.packages("maps")
+install.packages("maps")
 
 library(twitteR)
 library(streamR)
@@ -13,7 +13,7 @@ library(data.table)
 library(tm)
 
 
-setwd("~/Emotion_Analysis_TM")
+setwd("~/GitHub/Emotion_Analysis_TM")
 source('./my_oauth.r', encoding = 'latin1')
 
 
@@ -44,20 +44,24 @@ while(TRUE)
 
 
 
-str(data.tweet)
-
 ###Elimina duplicados
 data.tweet <- unique(data.tweet)
 
+
+
+
+data.tweet2 <- unique(data.tweet$text)
+ 
+str(data.tweet)
+
+###Formato Texto
+data.tweet$text=iconv(data.tweet$text,from="UTF-8",to="ASCII//TRANSLIT")
 
 
 ####calcula maximo RT y minimo RT####
 data.tweet[,min_RT:=min(retweet_count),by=text]
 data.tweet[,max_RT:=max(retweet_count),by=text]
 
-
-###Formato Texto
-data.tweet$text=iconv(data.tweet$text,from="UTF-8",to="ASCII//TRANSLIT")
 
 ##Remove double whitespaces
 data.tweet$text=stripWhitespace(data.tweet$text)
@@ -69,23 +73,44 @@ head(data.tweet$created_at,10)
 ###veo las variables
 str(data.tweet)
 
+####Borra variables
+data.tweet$expanded_url <- NULL
+data.tweet$url <- NULL
+data.tweet$user_url <- NULL
+data.tweet$description  <- NULL
+data.tweet$source  <- NULL
+
+
+####Limpieza####
+
+words <- c("aborto", "vida")
+tabla <- as.data.frame(sapply(words, grepl, data.tweet$text))
+
+data.tweet <- cbind(data.tweet, tabla)
+
+
+
+table(tabla$aborto)
+table(tabla$vida)
+
+
 ###selecciono solo los georeferenciados###
 data.tweet_georef<-subset(data.tweet, (!is.na(data.tweet$place_lat)))
+
+
+table(data.tweet_georef$aborto)
+table(data.tweet_georef$vida)
+
 
 ###Guardo la base de tweets
 write.table(data.tweet, file="./data.tweet.txt", row.names=FALSE, col.names=TRUE, quote=TRUE, sep="\t", eol = "\r\n")
 
 
 
-#leo el dataset 
-## ESTO ES UN CONTROL  NOMAS (SERIA BUENO HACERLO CON READ.TABLE, POR AHORA NO ME FUNCIONA)
-data_tweet_bla <- read_delim("~/Emotion_Analysis_TM/data.tweet.txt", "\t", escape_double = FALSE, trim_ws = TRUE)
-#####MAPA############
 
 
 map.data <- map_data("world")
 points <- data.frame(x = as.numeric(data.tweet_georef$place_lon), y = as.numeric(data.tweet_georef$place_lat))
-#points2 <- data.frame(x = as.numeric(tweets2vidas.df$place_lon), y = as.numeric(tweets2vidas.df$place_lat))
 ggplot(map.data) + geom_map(aes(map_id = region), map = map.data, fill = "white", 
                             color = "grey20", size = 0.25) + expand_limits(x = map.data$long, y = map.data$lat) + 
   theme(axis.line = element_blank(), axis.text = element_blank(), axis.ticks = element_blank(), 
@@ -93,7 +118,6 @@ ggplot(map.data) + geom_map(aes(map_id = region), map = map.data, fill = "white"
         panel.grid.major = element_blank(), plot.background = element_blank(), 
         plot.margin = unit(0 * c(-1.5, -1.5, -1.5, -1.5), "lines")) + 
   geom_point(data = points, aes(x = x, y = y), size = 3, alpha = 0.6, color = "dark green")
-#+geom_point(data = points2, aes(x = x, y = y), size = 2, alpha = 0.6, color = "light blue")
 
 
 
