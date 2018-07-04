22a11ea19cf624ab7765685c28004271a840de6c
Juan Pablo Sokil <juanpablosokil@gmail.com>
2018-07-03 23:32:33 -0300

    Add files via upload

--- 01-Procesa_Tweets.py
+++ 01-Procesa_Tweets.py
@@ -9,7 +9,6 @@ from sklearn.feature_extraction.text import CountVectorizer
 from datetime import datetime
 
 
-
 ###Levanta datos
 base_tweets1 = pd.read_csv("C:/Users/Juan/Documents/GitHub/Emotion_Analysis_TM/dataset/data.tweet_tabulador.txt", sep='\t', encoding='latin1', low_memory=False)
 base_tweets1.columns
@@ -34,32 +33,56 @@ base_tweets1['horario'] = base_tweets1['horario'] - pd.Timedelta('03:00:00')
 ###elimino segundos
 base_tweets1['horario'] = base_tweets1['horario'].apply(lambda t: t.replace(second=0))
 
+##SUBSET PARA TRABAJAR MAS FACIL
+###Quitar los duplicados, mantener el ultimo##
+unique_tweets=base_tweets1.groupby(['text']).min()
+unique_tweets=unique_tweets.reset_index()
+
+###convierte el df a csv
+
+unique_tweets.columns
+
+unique_tweets.drop(columns=['in_reply_to_user_id_str', 'lang',
+                            'favorited', 'truncated',
+       'id_str','in_reply_to_status_id_str','statuses_count',
+       'followers_count', 'favourites_count', 'protected', 'time_zone','user_lang'])
+
+unique_tweets.to_csv('./unique_tweets.txt',  header=True, sep='\t', encoding='latin1')
 
 
 
-##POR AHORA QUEDA PENDIENTE
-#localidades_reg_ex = base_tweets1_reduc.groupby('location').agg('size').sort_values(ascending=False)
-#nombres_reg_ex = base_tweets1_reduc.groupby('name').agg('size').sort_values(ascending=False)
 
 
+
+
+
+
+
+
+
+#####LEVANTAR DATOS#####
+base_tweets1 = pd.read_csv("C:/Users/Juan/Documents/GitHub/Emotion_Analysis_TM/dataset/unique_tweets.txt", sep='\t', encoding='latin1', low_memory=False)
+base_tweets1['horario']=pd.to_datetime(base_tweets1['horario'])
+base_tweets1['text']=base_tweets1['text'].str.lower()  
+        
+        
+        
+        
 #####Tweets por minutos###TOTAL - POSITIVOS - NEGATIVOS #
 base_tweets1.groupby('horario').agg('size').plot(color='black', label='Total')
-base_tweets1[base_tweets1['#AbortoLegalYa'] | base_tweets1['#AbortoLegalSeguroYGratuito'] | base_tweets1['#AbortoSeraLey'] | base_tweets1['#QueSeaLey'] | base_tweets1['#13JAbortoLegal'] | base_tweets1['#VotenAbortoLegal']  ].groupby('horario').agg('size').plot(color='green', label='A Favor')
-base_tweets1[base_tweets1['#SalvemosLas2Vidas'] | base_tweets1['#ArgentinaEsProVida']  | base_tweets1['#NoAlAbortoEnArgentina'] | base_tweets1['#SiALaVida'] | base_tweets1['#NoAlAbortoEnArgentina'] ].groupby('horario').agg('size').plot(color='blue', label='En Contra')                 
-#base_tweets1[base_tweets1['text'].str.contains('Carrio', 'carrio', regex=False)].groupby('horario').agg('size').plot(color='yellow', label='Carrio')
+base_tweets1[base_tweets1['text'].str.contains('#abortolegalta|#mediasancion|#abortolegalseguroygratuito|#quesealey|#abortoseraley|#abortolegalya|#nosotrasdecidimos|#abortolegaloclandestino|#13jabortolegal|#queelabortosealey',regex=True)].groupby('horario').agg('size').plot(color='green', label='A Favor')
+base_tweets1[base_tweets1['text'].str.contains('#provida|#salvemoslas2vidas|#salvemoslas2vidas|#noalabortoenargentina|#sialasdosvidas|#argentinaesprovida|#noalaborto|#cuidemoslas2vidas|#sialavida',regex=True)].groupby('horario').agg('size').plot(color='blue', label='En Contra')
 #base_tweets1[base_tweets1['text'].str.contains('olmedo', 'Olmedo' , regex=False)  ].groupby('horario').agg('size').plot(color='red', label='Olmedo')
 plt.legend(loc='upper left')
-plt.xticks([min(base_tweets1['horario']),min(base_tweets1['horario'])+pd.Timedelta('06:00:00'),min(base_tweets1['horario'])+pd.Timedelta('12:00:00'),min(base_tweets1['horario'])+pd.Timedelta('18:00:00'),max(base_tweets1['horario'])], rotation=20)
+plt.xticks(base_tweets1['horario'], rotation=20)
 plt.show()     
 
 
  
+totales=base_tweets1.groupby('horario').agg('size')                               
+favor=base_tweets1[base_tweets1['text'].str.contains('#abortolegalta|#mediasancion|#abortolegalseguroygratuito|#quesealey|#abortoseraley|#abortolegalya|#nosotrasdecidimos|#abortolegaloclandestino|#13jabortolegal|#queelabortosealey',regex=True)].groupby('horario').agg('size')
+contra=base_tweets1[base_tweets1['text'].str.contains('#provida|#salvemoslas2vidas|#salvemoslas2vidas|#noalabortoenargentina|#sialasdosvidas|#argentinaesprovida|#noalaborto|#cuidemoslas2vidas|#sialavida',regex=True)].groupby('horario').agg('size')
 
-
-totales=base_tweets1.groupby('horario').agg('size')
-favor=base_tweets1[base_tweets1['#AbortoLegalYa'] | base_tweets1['#AbortoLegalSeguroYGratuito'] | base_tweets1['#AbortoSeraLey'] | base_tweets1['#QueSeaLey'] |base_tweets1['#13JAbortoLegal'] | base_tweets1['#VotenAbortoLegal']  ].groupby('horario').agg('size')
-contra=base_tweets1[base_tweets1['#SalvemosLas2Vidas'] | base_tweets1['#ArgentinaEsProVida']  | base_tweets1['#NoAlAborto'] | base_tweets1['#SiALaVida']  | base_tweets1['#NoAlAbortoEnArgentina']  ].groupby('horario').agg('size')
-                               
                                  
 ###junta los datos en un nuevo df                                 
 result = pd.concat([totales,favor,contra],axis=1)
@@ -73,26 +96,24 @@ result['pct_contra']=result['Contra']/result['Total']
 
 
 ##SUBSET PARA TRABAJAR MAS FACIL
-base_tweets1=base_tweets1.head(n=100)
+base_tweets1=base_tweets1.head(n=10000)
 
 
 ####PREPROCESAMIENTO####
-##pasa todo a minuscula
-base_tweets1['text']=base_tweets1['text'].str.lower()
 
 ###Cargo stopwords
+
+base_tweets1['text_punct'] = base_tweets1['text'].str.replace('[^\w\s]','')
+
 from nltk.corpus import stopwords
 stopwords=stopwords.words('spanish')
-base_tweets1['text_stopwords'] = base_tweets1['text'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stopwords)]))
-print(base_tweets1['text'][2])
-print(base_tweets1['text_stopwords'][2])
+base_tweets1['text_stopwords'] = base_tweets1['text_punct'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stopwords)]))
+
 
 ###Aplica stemmer (No es muy lindo - habria que probar un lematizador)
 from nltk.stem import SnowballStemmer
 stemmer_spanish = SnowballStemmer('spanish')
 base_tweets1['text_stopwords_stem'] = base_tweets1['text_stopwords'].apply(lambda x: ' '.join([stemmer_spanish.stem(word) for word in x.split()]))
-print(base_tweets1['text_stopwords'][2])
-print(base_tweets1['text_stopwords_stem'][2])
 
 
 ###Elimina usuarios, paginas y rt
@@ -101,21 +122,43 @@ base_tweets1['text_stopwords_stem2'] = base_tweets1['text_stopwords_stem2'].appl
 base_tweets1['text_stopwords_stem2'] = base_tweets1['text_stopwords_stem2'].apply(lambda x: ' '.join([word for word in x.split() if not  word.startswith('rt')]))
 
 
-print(base_tweets1['text_stopwords_stem2'][2])
-print(base_tweets1['text_stopwords_stem'][2])
+###Contador de palabras - aca deberia eliminar las palabras que tienen un recuento menor a 5
+df=pd.DataFrame(base_tweets1['text_stopwords_stem2'])
+contador_palabras=df.text_stopwords_stem2.apply(lambda x: pd.value_counts(x.split(" "))).sum(axis = 0)
+#Selecciona las que tienen mas de 5
+contador_palabras=contador_palabras[(contador_palabras  <5)]
+
+contador_palabras=contador_palabras.reset_index()
+
+
+##genera una lista
+palabras=contador_palabras['index'].tolist()
+base_tweets1['text_final'] = base_tweets1['text_stopwords_stem2'].apply(lambda x: ' '.join([word for word in x.split() if word not in (palabras)]))
+
+
+###ver ejemplos
+i=711
+print(base_tweets1['text'][i])
+print(base_tweets1['text_punct'][i])
+print(base_tweets1['text_stopwords'][i])
+print(base_tweets1['text_stopwords_stem'][i])
+print(base_tweets1['text_stopwords_stem2'][i])
+print(base_tweets1['text_final'][i])
+
+
+###Aplico
 
 
 
 ###Arma el tokenizer de twitter 
 tw = TweetTokenizer()
 ##lo aplica a los tweets completos- no la uso - lo hace pero no me sirve para el vectorizer
-tokens=base_tweets1['text_stopwords_stem2'].apply(tw.tokenize)
+tokens=base_tweets1['text_final'].apply(tw.tokenize)
 se = pd.Series(tokens)
 base_tweets1['tokenized'] = pd.DataFrame(se.values)
 
-print(base_tweets1['text_stopwords_stem2'][2])
+print(base_tweets1['text_final'][2])
 print(base_tweets1['tokenized'][2])
-
 
 
 ###ARMO EL BAG OF WORDS
@@ -124,48 +167,7 @@ tf=base_tweets1['tokenized'].apply(pd.value_counts).fillna(0).astype(int)
 
 
 
-
-
-
-
-tf_vectorizer = CountVectorizer(analyzer = "word", tokenizer = None, min_df=5, max_df=0.70, ngram_range=(1,2), stop_words = 'english', max_features = no_features)
-tf = tf_vectorizer.fit_transform(base_genero['Resumen_lematizado'])
-bagofwords= pd.DataFrame(tf.toarray(),columns=tf_vectorizer.get_feature_names())
-bagofwords.shape
-
-
-
-
-
-
-
-
-
-
-###### HASTA ACA ESTA BIEN#####
-
-
-
-from sklearn.feature_extraction.text import CountVectorizer
-no_features=1000
-#Armamos el vectorizer 
-
-vectorizer = CountVectorizer(analyzer = "word",
-                #min_df=10, 
-                ngram_range=(1,2),
-                #tokenizer=tw,
-                token_pattern='[a-zA-Z0-9]{3,}',
-                 max_features = no_features, tokenizer=tw.tokenize(tipo))
-
-###Aplica el modelo a los datos, en el ejemplo ejecuto una sola fila (para que salga rapido)
-tf = vectorizer.fit_transform(base_tweets1['text'].astype(str))    
-tf = vectorizer.fit_transform(base_tweets1['text'].apply(tw.tokenize)[:2])
-
-#Genera el vocabulario
-tf_feature_names = vectorizer.get_feature_names()
-vocab = vectorizer.get_feature_names()
-len(vocab)
-
+###############3
 
 
 
@@ -218,19 +220,6 @@ pprint(lda_model.get_params())
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 #####GRID PARAMETROS OPTIMOS#####
 
 # Define Search Param
@@ -243,7 +232,7 @@ lda = LatentDirichletAllocation()
 model = GridSearchCV(lda, param_grid=search_params)
 
 # Do the Grid Search
-model.fit(data_vectorized)
+model.fit(tf)
 
 
 
@@ -271,7 +260,7 @@ print("Best Model's Params: ", model.best_params_)
 print("Best Log Likelihood Score: ", model.best_score_)
 
 # Perplexity
-print("Model Perplexity: ", best_lda_model.perplexity(data_vectorized))
+print("Model Perplexity: ", best_lda_model.perplexity(tf))
 
 
 
@@ -295,10 +284,110 @@ plt.show()
 
 
 
+#############VER ESTO, ES UN PROBLEMITA
+best_lda_model.n_topics=10
 
 
 
-http://cfss.uchicago.edu/fall2016/webdata_lab07b.html
+# Create Document - Topic Matrix
+lda_output = best_lda_model.transform(tf)
+
+# column names
+topicnames = ["Topic" + str(i) for i in range(best_lda_model.n_topics)]
+topicnames = ["Topic" + str(i) for i in range(best_lda_model.n_topics)]
 
 
-##https://stackoverflow.com/questions/44173624/how-to-nltk-word-tokenize-to-a-pandas-dataframe-for-twitter-data
+
+# index names
+docnames = ["Doc" + str(i) for i in range(10000)]
+
+# index names
+docnames = ["Doc" + str(i) for i in range(10000)]
+
+
+# Make the pandas dataframe
+df_document_topic = pd.DataFrame(np.round(lda_output, 2), columns=topicnames, index=docnames)
+
+# Get dominant topic for each document
+dominant_topic = np.argmax(df_document_topic.values, axis=1)
+df_document_topic['dominant_topic'] = dominant_topic
+
+# Styling
+def color_green(val):
+    color = 'green' if val > .1 else 'black'
+    return 'color: {col}'.format(col=color)
+
+def make_bold(val):
+    weight = 700 if val > .1 else 400
+    return 'font-weight: {weight}'.format(weight=weight)
+
+# Apply Style
+df_document_topics = df_document_topic.head(15).style.applymap(color_green).applymap(make_bold)
+df_document_topics
+
+df_topic_distribution = df_document_topic['dominant_topic'].value_counts().reset_index(name="Num Documents")
+df_topic_distribution.columns = ['Topic Num', 'Num Documents']
+df_topic_distribution
+
+
+
+####VER####
+pyLDAvis.enable_notebook()
+panel = pyLDAvis.sklearn.prepare(best_lda_model, tf, tf, mds='tsne')
+panel
+
+pyLDAvis.save_html(panel,'../resultados/vis_sklearn.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Topic-Keyword Matrix
+df_topic_keywords = pd.DataFrame(best_lda_model.components_)
+# Assign Column and Index
+df_topic_keywords.columns = vectorizer.get_feature_names()
+df_topic_keywords.index = topicnames
+# View
+df_topic_keywords.head()
+
+
+
+
+
+# Show top n keywords for each topic
+def show_topics(vectorizer=vectorizer, lda_model=lda_model, n_words=20):
+    keywords = np.array(vectorizer.get_feature_names())
+    topic_keywords = []
+    for topic_weights in lda_model.components_:
+        top_keyword_locs = (-topic_weights).argsort()[:n_words]
+        topic_keywords.append(keywords.take(top_keyword_locs))
+    return topic_keywords
+
+topic_keywords = show_topics(vectorizer=vectorizer, lda_model=best_lda_model, n_words=15)        
+
+# Topic - Keywords Dataframe
+df_topic_keywords = pd.DataFrame(topic_keywords)
+df_topic_keywords.columns = ['Word '+str(i) for i in range(df_topic_keywords.shape[1])]
+df_topic_keywords.index = ['Topic '+str(i) for i in range(df_topic_keywords.shape[0])]
+df_topic_keywords
+
+
+
+
+
+
+
+
+
+
+
+
